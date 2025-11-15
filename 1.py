a9dfe8fa81a6d9f4284d8592e87ee65c50bfe2c1
Xiao22p <19866817908@163.com>
2025-11-15 13:55:46 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+xname = 'peng'
+mname = 'haohui'
+name = f"{xname} {mname}"
+message = f"Hello,{name.title()}!"
+print(message)
---
+++ name_case1.py
@@ -0,0 +1,9 @@
+name = "PENG HAOHUI"
+print(name.lower()) # 全小写lower()方法
+
+name1 = 'peng haohui'
+print(name1.upper()) # 全大写upper()方法
+
+name2 = 'peng haohui'
+print(name2.title()) # 首字母大写显示title()方法
+
